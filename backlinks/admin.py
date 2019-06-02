d8ec88d6b2f0e12955b55ddbf4fd018ab57a3c46
Anima <raventailminer@gmail.com>
2019-06-02 16:45:51 +0300

    d

--- backlinks/admin.py
+++ backlinks/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from .models import Backlinks
-import models
 
 
-@admin.register(models.Backlinks)
 class AuthorAdmin(admin.Backlinks):
   date_hierarchy = 'created_date'
   list_display = ('link','keyword','author')
